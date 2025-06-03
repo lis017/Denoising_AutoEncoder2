114e41e4d9273d9edbfbeb58c7e1141e55bf0822
lis017 <minhyeok12.dev@gmail.com>
2025-06-04 01:43:54 +0900

    과제3 성공한 코드

--- AE_test.py
+++ AE_test.py
@@ -11,7 +11,8 @@ if __name__ == "__main__":
     data_loader = MNISTData()
     data_loader.load_data()
     x_train = data_loader.x_train
-    x_test_noisy = MNISTData.add_noise(data_loader.x_test)
+    x_test = data_loader.x_test
+    y_test = data_loader.y_test
     input_output_dim = data_loader.in_out_dim
 
     auto_encoder = AutoEncoder()
@@ -20,42 +21,36 @@ if __name__ == "__main__":
     print("load model weights from %s" % load_path)
     auto_encoder.load_weights(load_path)
 
-    # print for test
-    num_test_items = 56
-    test_data = data_loader.x_test[0:num_test_items, :]
-    test_label = data_loader.y_test[0:num_test_items]
-    test_data_x_print = test_data.reshape(num_test_items, data_loader.width, data_loader.height)
+    # (1) 1000개 원본 테스트 이미지 중 각 class별 평균 latent vector 구하기
+    num_sample = 1000
+    x_test_sample = x_test[:num_sample]  # 노이즈 없음 (원본)
+    y_test_sample = y_test[:num_sample]
 
-    print("const by codes")
-    print("x_test_noisy shape:", x_test_noisy.shape)
+    # Encoder를 통과시켜 latent vector 얻기
+    latent_vecs = auto_encoder.encoder.predict(x_test_sample)
+    print("latent_vecs shape:", latent_vecs.shape)  # (1000, latent_dim)
 
-    reconst_data = auto_encoder.en_decoder.predict(x_test_noisy)
-    print("reconst_data shape:", reconst_data.shape)
+    # 평균을 위한 준비
+    num_classes = 10
+    latent_dim = latent_vecs.shape[1]
+    avg_codes = np.zeros((num_classes, latent_dim))
+    count_per_class = np.zeros(num_classes)
 
-    reconst_data_subset = reconst_data[:num_test_items]  # 56개만 추출
-    reconst_data_x_print = reconst_data_subset.reshape(num_test_items, data_loader.width, data_loader.height)
-    reconst_data_x_print = tf.math.sigmoid(reconst_data_x_print)
-    MNISTData.print_56_pair_images(test_data_x_print, reconst_data_x_print, test_label)
-
-
-
-
-    print("const by code means for each digit")
-    avg_codes = np.zeros([10, 32])
-    avg_add_cnt = np.zeros([10])
-
-    latent_vecs = auto_encoder.encoder.predict(x_test_noisy)
-    for i, label in enumerate(test_label):
+    for i in range(num_sample):
+        label = y_test_sample[i]
         avg_codes[label] += latent_vecs[i]
-        avg_add_cnt[label] += 1.0
+        count_per_class[label] += 1
 
-    for i in range(10):
-        if avg_add_cnt[i] > 0.1:
-            avg_codes[i] /= avg_add_cnt[i]
+    # 평균 계산
+    for i in range(num_classes):
+        if count_per_class[i] > 0:
+            avg_codes[i] /= count_per_class[i]
 
-    avg_code_tensor = tf.convert_to_tensor(avg_codes)
-    reconst_data_by_vecs = auto_encoder.decoder.predict(avg_code_tensor)
-    reconst_data_x_by_mean_print = reconst_data_by_vecs.reshape(10, data_loader.width, data_loader.height)
-    label_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    MNISTData.print_10_images(reconst_data_x_by_mean_print, label_list)
-    print("reconst_data shape:", reconst_data.shape)
+    # (2) 평균 latent vector를 Decoder에 넣어 이미지 생성
+    avg_code_tensor = tf.convert_to_tensor(avg_codes, dtype=tf.float32)
+    reconst_from_avg = auto_encoder.decoder.predict(avg_code_tensor)
+
+    # 이미지 출력용 reshape
+    reconst_images = reconst_from_avg.reshape(num_classes, data_loader.width, data_loader.height)
+    label_list = list(range(10))
+    MNISTData.print_10_images(reconst_images, label_list)
